22b26600237d06c72a20e99e60fe08e602d63d77
Aniumbott <aniketnareshkumarrana@gmail.com>
2023-03-25 20:47:26 +0530

    node Tree done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NodesTree from "./components/NodeTree";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <D3 />
-      </header>
+      <main>
+        <NodesTree />
+      </main>
     </div>
   );
 }
